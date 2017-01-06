a6df813b5afb895fe011030d982a7b4c364a836f
Maciej Kotowski <maciej.kotowski0@gmail.com>
2017-01-06 21:23:37 +0100

    contactNotFound exception

---
+++ src/main/java/pl/volanto/exception/ContactNotFoundException.java
@@ -0,0 +1,10 @@
+package pl.volanto.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, 
+reason = "User does not exist!")
+public class ContactNotFoundException extends RuntimeException {
+
+}
