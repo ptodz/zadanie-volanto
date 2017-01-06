551856c7b6143efb948476c7dc7fb7841e896789
Maciej Kotowski <maciej.kotowski0@gmail.com>
2017-01-06 14:02:26 +0100

    added mapper + dto's

---
+++ src/main/java/pl/volanto/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package pl.volanto.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+
+	private Long id;
+
+	private String login;
+
+	private String password;
+
+	List<ContactDTO> contacts = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<ContactDTO> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<ContactDTO> contacts) {
+		this.contacts = contacts;
+	}
+}
